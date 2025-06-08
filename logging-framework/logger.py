08978cb1ed7b04e85e42a020c15474870928242b
SJRiz <rizvij006@gmail.com>
2025-06-07 22:48:18 -0400

    Functions are timed, and logs are in a seperate folder

--- logging-framework/logger.py
+++ logging-framework/logger.py
@@ -1,31 +1,57 @@
-import functools
 import logging
 import os
+import time
+from functools import wraps
+from datetime import date
 
-# Gets the path to the logging-framework folder
-script_dir = os.path.dirname(os.path.abspath(__file__))
-# Put the logs.txt in this folder
-log_path = os.path.join(script_dir, "logs.txt")
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', filename=log_path, encoding='utf-8', level=logging.DEBUG)
-
-# Clear the logs.txt or make a new one if it doesnt exist
 if __name__ == "__main__":
-    with open(log_path, "w") as logs:
-        logs.write("")
+
+    # Gets the path to the logs folder
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    logs_folder = os.path.join(script_dir, "logs")
+
+    # Create a new txt file path in the folder
+    log_file_path = os.path.join(logs_folder, f'logs-{date.today()}.txt')
+
+    # Configure the logger to write in the new logs file
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', filename=log_file_path, encoding='utf-8', level=logging.DEBUG)
+
+    with open(log_file_path, "a") as logs:
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        logs.write(f'\n--------------- Execution Started at {current_time} -----------------\n')
 
 # Decorator to log the function call
 def log_function_call(func):
-    @functools.wraps(func)
+    @wraps(func)
     def wrapper(*args, **kwargs):
         logger.info(f'Running function \'{func.__name__}\' with arguments {args} and keyword arguments {kwargs}')
-        return_val = func(*args, **kwargs)
-        logger.info(f'Function \'{func.__name__}\' finished and returned {return_val}')
+        start_time = time.time()
+
+        try:
+            return_val = func(*args, **kwargs)
+            logger.info(f'Function \'{func.__name__}\' returned {return_val}.')
+        except Exception as err:
+            logger.critical(f'Function \'{func.__name__}\' failed with error message: {err}.')
+        finally:
+            logger.info(f'Function \'{func.__name__}\' finished with {time.time() - start_time}s runtime.')
+
     return wrapper
+
+########################################################################################################
 
 @log_function_call
 def add(x, y):
+    for _ in range(10000000):
+        pass
+    return x + y
+
+@log_function_call
+def add2(x, y):
+    for i in range(10000000):
+        pass
     return x + y
 
 add(3,4)
+add2(3)
