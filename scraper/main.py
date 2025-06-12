8f63bec12b0a51b83ac5bf84f5ac66a4f294f861
SJRiz <rizvij006@gmail.com>
2025-06-11 20:34:45 -0400

    Finished base code for the scraper

--- scraper/main.py
+++ scraper/main.py
@@ -1,4 +1,28 @@
-def func():
-    yield 1
+import requests
+import time
+from bs4 import BeautifulSoup
 
-print(list(func()))
+def get_html(link):
+    """Function that gets the HTML of a response"""
+    try:
+        response = requests.get(link, timeout=5)
+        response.raise_for_status()
+        return response.text
+    except Exception as err:
+        print(f'Error fetching {link}: {err}')
+        return None
+    
+def parse_html(html):
+    """Function that parses the HTML and looks for titles, along with their links"""
+    soup = BeautifulSoup(html, "html.parser")
+    for item in soup.select('.titleline > a'):
+        yield {
+            "title": item.text.strip(),
+            "link": item["href"]
+        }
+
+def scrape(link):
+    """Function that scrapes data from a link by using get_html and parse_html"""
+    html = get_html(link)
+    if html:
+        return list(parse_html(html))
